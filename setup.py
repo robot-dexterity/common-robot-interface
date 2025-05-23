c03deb95e56773b0d43ce453e50ab017698c45a0
Richard Lane <mh19137@bristol.ac.uk>
2025-05-23 13:38:21 +0100

    include cri/dummy in newest pypi release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="common_robot_interface",
-    version="0.5.0",
+    version="0.5.1",
     description="Common Robot Interface",
     license="GPLv3",
     long_description=long_description,
@@ -17,7 +17,7 @@ setup(
     author="John Lloyd, Nathan Lepora",
     author_email="j.lloyd@bristol.ac.uk, n.lepora@bristol.ac.uk",
     url="https://github.com/robot-dexterity/common-robot-interface",
-    packages=["cri", "cri.abb", "cri.ur", "cri.ur.rtde", "cri.dobot", "cri.dobot.mg400",  "cri.dobot.cr", "cri.dobot.magician"],
+    packages=["cri", "cri.abb", "cri.ur", "cri.ur.rtde", "cri.dobot", "cri.dobot.mg400",  "cri.dobot.cr", "cri.dobot.magician", "cri.dummy"],
 	package_data={'cri.ur': ['rtde_config.xml'],
             "cri.dobot.magician": ['DobotDll.dll',"msvcp120.dll","msvcr120.dll","Qt5Core.dll","Qt5Network.dll","Qt5SerialPort.dll"]},
     install_requires=["numpy", "transforms3d"],
